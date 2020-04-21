b1f2928e1d4c5d895481d7875cada0c6d2531f1c
Devlin-Moyer <devmoy@gmail.com>
2020-04-21 19:42:42 -0400

    Figure 3 includes data from pruned networks

---
+++ scripts/figure_3.R
@@ -0,0 +1,118 @@
+# figure_3.R
+# make the plots for figure 3 that show sizes of string chemistry networks
+# as functions of the number of unique monomers and the maximum string length
+# used to generate them
+
+# load package(s)
+library(tidyverse)
+
+# make all plots be nice
+theme_set(theme_bw())
+
+# read in data
+counts <- read.csv(
+  "../data/figure_3_data.csv", header = F,
+  col.names = c("monos", "max_len", "mets", "rxns")
+) %>%
+  mutate(monos = as.factor(monos)) %>%
+  filter(rxns > 0) %>%
+  mutate(ratio = rxns/mets)
+
+# find all ratios files
+all_files <- list.files("data/")
+ratio_files <- all_files[grepl("ratios.csv", all_files)]
+
+# parse information from all of those files into a nice dataframe
+parse_ratio_file <- function(ratio_file) {
+  # get mean and standard deviation of reaction-to-metabolite ratios
+  ratio_dist <- read.csv(paste("data/", ratio_file, sep = ""), header = F)
+  mean_ratio <- mean(ratio_dist$V1)
+  ratio_sd <- sd(ratio_dist$V1)
+  # get number of monomers and max length used to generate the universal
+  # network all these ratios correspond to
+  filename_bits <- strsplit(ratio_file, "_")[[1]]
+  monos <- filename_bits[1]
+  max_len <- filename_bits[2]
+  return(c(monos, max_len, mean_ratio, ratio_sd))
+}
+
+ratio_df <- as.data.frame(t(sapply(ratio_files, parse_ratio_file)))
+# make the dataframe a bit easier to work with
+colnames(ratio_df) <- c("monos", "max_len", "mean_ratio", "ratio_sd")
+ratio_df <- ratio_df %>%
+  mutate_at(-monos, function(col) as.numeric(as.character(col))) %>%
+  # for plotting purposes we only need to know how many monomers there were,
+  # not exactly what they were
+  mutate(monos = nchar(as.character(monomers))
+
+# googled these so they're hard-coded
+ecoli_mets <- 1805
+ecoli_rxns <- 2583
+kegg_mets <- 18686
+kegg_rxns <- 11395
+
+# metabolite count as a function of monomers and max length
+png("data/figure_3_met.png")
+counts %>%
+  ggplot(aes(x = max_len, y = mets)) + 
+    geom_line(aes(color = monos)) + 
+    scale_x_continuous(breaks = c(2,4,6,8,10)) +
+    scale_y_continuous(trans = "log10") +
+    geom_hline(aes(yintercept = ecoli_mets)) +
+    annotate("text", x = 3, y = 2700, label = "E. coli Metabolites") +
+    geom_hline(aes(yintercept = kegg_mets)) +
+    annotate("text", x = 3, y = 30000, label = "KEGG Metabolites") +
+    labs(
+      x = "Maximum String Length", y = "Number of Metabolites", 
+      color = "Types of Monomers", 
+      title = "Network Sizes By Metabolite Count"
+    )
+dev.off()
+
+# reaction count as a function of monomers and max length
+png("data/figure_3_rxn.png")
+counts %>%
+  ggplot(aes(x = max_len, y = rxns)) + 
+    geom_line(aes(color = monos)) + 
+    scale_x_continuous(breaks = c(2,4,6,8,10)) +
+    scale_y_continuous(trans = "log10") +
+    geom_hline(aes(yintercept = ecoli_rxns)) +
+    annotate("text", x = 3, y = 4000, label = "E. coli Reactions") +
+    geom_hline(aes(yintercept = kegg_rxns)) +
+    annotate("text", x = 3, y = 20000, label = "KEGG Reactions") +
+    labs(
+      x = "Maximum String Length", y = "Number of Reactions", 
+      color = "Types of Monomers", 
+      title = "Network Sizes By Reaction Count"
+    )
+dev.off()
+
+# reaction-to-metabolite ratio as a function of monomers and max length
+png("data/figure_3_ratio.png")
+ggplot() +
+  # plot ratios from universal networks
+  geom_line(data = counts, aes(x = max_len, y = ratio, color = monos)) +
+  # plot ratios from pruned networks
+  geom_point(data = ratio_df, aes(x = max_len, y = mean_ratio, color = monos)) +
+  geom_errorbar(
+    data = ratio_df, 
+    aes(
+      x = max_len, y = ratio, color = monos,
+      ymin = mean_ratio-ratio_sd, ymax = mean_ratio+ratio_sd
+    )
+  )
+  # set x-axis tick spacing
+  scale_x_continuous(breaks = c(2,4,6,8,10)) +
+  # add horizontal line for E. coli ratio
+  geom_hline(aes(yintercept = ecoli_rxns/ecoli_mets)) +
+  annotate("text", x = 8, y = 1.75, label = "E. coli Ratio") +
+  # add horizontal line for KEGG ratio
+  geom_hline(aes(yintercept = kegg_rxns/kegg_mets)) +
+  annotate("text", x = 8, y = 0.9, label = "KEGG Ratio") +
+  labs(
+    x = "Maximum String Length", 
+    y = "Number of Reactions / Number of Metabolites", 
+    color = "Types of Monomers", 
+    title = "Network Sizes By Ratio of Reaction Count to Metabolite Count"
+  )
+dev.off()
--- scripts/rxn_met_ratio_data.py
+++ scripts/rxn_met_ratio_data.py
@@ -23,6 +23,27 @@ SCN = scn.CreateNetwork(monos, int(max_pol))
 cobra_model = scn.make_cobra_model(SCN.met_list, SCN.rxn_list)
 scn.reverse_rxns(cobra_model, len(cobra_model.reactions))
 
+# generate all of the environments
+envs = list()
+i = 0
+# count how many consecutive times this fails to find a new environment
+fail = 0
+while i < int(env_count):
+    i += 1
+    env = random.sample(cobra_model.metabolites, int(ins))
+    # make sure this is a new environment
+    if env not in envs:
+        envs.append(env)
+        # reset fail counter
+        fail = 0
+    else:
+        i -= 1
+        fail += 1
+    if fail > 1000:
+        print('Failed to find a new environment 1000 times in a row.')
+        print(f'Proceeding with only {len(envs)} environments')
+        break
+
 # don't bother storing any information besides the reaction to metabolite ratio
 ratios = list()
 
@@ -37,37 +58,39 @@ while bm_trial < int(bm_count):
     cobra_model.objective = bm_rxn
     # loop over number of different environments to use
     # use a counter so we can redo runs if we get an infeasible solution
-    env = 0
-    # also have a counter for infeasible solutions so that we make sure we
-    # don't spend forever trying to find solutions for a biomass reaction
-    # that is just not particularly feasible
-    infeas_count = 0
-    while env < int(env_count):
-        env += 1
-        if env % 100 == 0:
-            print(f'On environment {env}')
-        # get an environment and do FBA
-        scn.choose_inputs(int(ins), cobra_model, bm_rxn)
+    env_trial = 0
+    # just for fun, keep a counter of how many times pruning actually happened
+    prune_count = 0
+    for env in envs:
+        env_trial += 1
+        if env_trial % 100 == 0:
+            print(f'On environment {env_trial}')
+        # add appropriate input reactions and do FBA
+        for met in env:
+            in_rxn = cobra.Reaction(
+                '->' + met.id,
+                upper_bound = 100.0, # only allow importing of this metabolite
+                lower_bound = 0.0
+            )
+            in_rxn.add_metabolites({met: 1.0})
+            cobra_model.add_reaction(in_rxn)
         solution = cobra_model.optimize()
         bm_rxn_flux = solution.fluxes.get(key = bm_rxn.id)
         # don't bother pruning if there is no feasible solution on the full net
         if solution.status == 'infeasible' or bm_rxn_flux < 10e-10:
-            # but also don't count this run
-            env -= 1
-            infeas_count += 1
+            # don't add anything to ratios
+            pass
         else:
-            #print(solution.fluxes.get(key = bm_rxn.id))
+            prune_count += 1
             # do min flux pruning and get reaction to metabolite ratio
             pruned = scn.min_flux_prune(cobra_model, bm_rxn)
             ratio = len(pruned.reactions)/len(pruned.metabolites)
             ratios.append(ratio)
         # remove input reactions in preparation for next round of pruning
         cobra_model.remove_reactions(cobra_model.boundary)
-        # if 1000 environments have not produced growth with this biomass
-        # reaction it's garbage get a new one
-        if infeas_count > 1000:
-            bm_trial -= 1
-            break
+    # just for fun, print how many times pruning was successful
+    print(f'This biomass reaction could sustain growth in {prune_count} ' +
+    f'out of {len(envs)} environments tested.')
 
 output = '\n'.join([str(x) for x in ratios]) + '\n'
 with open(f'data/{monos}_{max_pol}_{ins}ins_{outs}outs_{env_count}envs_' + 
